8746eab1804e6eda32ded3e9f83f4602c698ea1f
Gingeh <39150378+Gingeh@users.noreply.github.com>
2025-10-14 10:30:13 +1100

    irc-colors/annoyances: use prettier for formatting

--- plugins/annoyances/index.tsx
+++ plugins/annoyances/index.tsx
@@ -1,106 +1,168 @@
 /// <reference path="../../node_modules/@uwu/shelter-defs/dist/shelter-defs/rootdefs.d.ts" />
 
 const {
-    plugin: { store },
-    ui: { SwitchItem },
-    observeDom,
+  plugin: { store },
+  ui: { SwitchItem },
+  observeDom,
 } = shelter;
 
 const hopefully_unique_id = "annoyances-gingeh";
 
 export const settings = () => (
-    <>
-        <SwitchItem value={store.member_list_backgrounds} onChange={(v: boolean) => { store.member_list_backgrounds = v; }}>
-            Remove member list backgrounds
-        </SwitchItem>
-        <SwitchItem value={store.avatar_decorations} onChange={(v: boolean) => { store.avatar_decorations = v; }}>
-            Remove avatar decorations
-        </SwitchItem>
-        <SwitchItem value={store.fancy_profile_themes} onChange={(v: boolean) => { store.fancy_profile_themes = v; }}>
-            Remove fancy profile themes
-        </SwitchItem>
-        <SwitchItem value={store.gift_button} onChange={(v: boolean) => { store.gift_button = v; }}>
-            Remove gift button
-        </SwitchItem>
-        <SwitchItem value={store.quests_button} onChange={(v: boolean) => { store.quests_button = v; }}>
-            Remove quests button
-        </SwitchItem>
-        <SwitchItem value={store.boosts_button} onChange={(v: boolean) => { store.boosts_button = v; }}>
-            Remove boosts button and progress in channel list
-        </SwitchItem>
-    </>
-)
+  <>
+    <SwitchItem
+      value={store.member_list_backgrounds}
+      onChange={(v: boolean) => {
+        store.member_list_backgrounds = v;
+      }}
+    >
+      Remove member list backgrounds
+    </SwitchItem>
+    <SwitchItem
+      value={store.avatar_decorations}
+      onChange={(v: boolean) => {
+        store.avatar_decorations = v;
+      }}
+    >
+      Remove avatar decorations
+    </SwitchItem>
+    <SwitchItem
+      value={store.fancy_profile_themes}
+      onChange={(v: boolean) => {
+        store.fancy_profile_themes = v;
+      }}
+    >
+      Remove fancy profile themes
+    </SwitchItem>
+    <SwitchItem
+      value={store.gift_button}
+      onChange={(v: boolean) => {
+        store.gift_button = v;
+      }}
+    >
+      Remove gift button
+    </SwitchItem>
+    <SwitchItem
+      value={store.quests_button}
+      onChange={(v: boolean) => {
+        store.quests_button = v;
+      }}
+    >
+      Remove quests button
+    </SwitchItem>
+    <SwitchItem
+      value={store.boosts_button}
+      onChange={(v: boolean) => {
+        store.boosts_button = v;
+      }}
+    >
+      Remove boosts button and progress in channel list
+    </SwitchItem>
+  </>
+);
 
 let cleanup_callbacks: (() => void)[] = [];
 
 export function onLoad() {
-    // member list backgrounds
-    store.member_list_backgrounds ??= true;
-    if (store.member_list_backgrounds) {
-        cleanup_callbacks.push(observeDom("[class*=nameplated__] > [class*=container__]", (elem) => {
+  // member list backgrounds
+  store.member_list_backgrounds ??= true;
+  if (store.member_list_backgrounds) {
+    cleanup_callbacks.push(
+      observeDom("[class*=nameplated__] > [class*=container__]", (elem) => {
+        elem.style.display = "none";
+      })
+    );
+  }
+
+  // avatar decorations
+  store.avatar_decorations ??= true;
+  if (store.avatar_decorations) {
+    cleanup_callbacks.push(
+      observeDom("[class*=avatarDecoration__]", (elem) => {
+        elem.style.display = "none";
+      })
+    );
+  }
+
+  // fancy profile themes
+  store.fancy_profile_themes ??= true;
+  if (store.fancy_profile_themes) {
+    cleanup_callbacks.push(
+      observeDom(
+        `.custom-theme-background:not([data-${hopefully_unique_id}])`,
+        (elem) => {
+          elem.classList.remove("custom-theme-background");
+          elem.classList.remove("custom-user-profile-theme");
+          elem.style =
+            "--profile-gradient-primary-color: var(--background-surface-high);" +
+            "--profile-gradient-secondary-color: var(--background-surface-high);" +
+            "--profile-gradient-overlay-color: rgba(0, 0, 0, 0);" +
+            "--profile-gradient-button-color: var(--background-mod-subtle);" +
+            "--profile-gradient-modal-background-color: var(--background-base-lower);";
+
+          const mask_circle = elem.querySelector("circle");
+          if (mask_circle) {
+            mask_circle.setAttribute("cx", "56");
+            mask_circle.setAttribute("cy", "101");
+          }
+
+          elem.setAttribute(`data-${hopefully_unique_id}`, "true");
+        }
+      )
+    );
+  }
+
+  // gift button
+  store.gift_button ??= true;
+  if (store.gift_button) {
+    cleanup_callbacks.push(
+      observeDom("[aria-label*=gift][class*=button__]", (elem) => {
+        elem.style.display = "none";
+      })
+    );
+  }
+
+  // quests button
+  store.quests_button ??= true;
+  if (store.quests_button) {
+    cleanup_callbacks.push(
+      observeDom("[href*=quest-home]", (elem) => {
+        elem.style.display = "none";
+      })
+    );
+  }
+
+  // boosts button in channel list
+  store.boosts_button ??= true;
+  if (store.boosts_button) {
+    cleanup_callbacks.push(
+      observeDom(
+        `[class*=basicChannelRowLink__]:not([data-${hopefully_unique_id}])`,
+        (elem) => {
+          if (
+            elem
+              .querySelector("[class*=name__] span")
+              ?.textContent.includes("Boosts")
+          ) {
             elem.style.display = "none";
-        }));
-    }
-
-    // avatar decorations
-    store.avatar_decorations ??= true;
-    if (store.avatar_decorations) {
-        cleanup_callbacks.push(observeDom("[class*=avatarDecoration__]", (elem) => {
-            elem.style.display = "none";
-        }));
-    }
-
-    // fancy profile themes
-    store.fancy_profile_themes ??= true;
-    if (store.fancy_profile_themes) {
-        cleanup_callbacks.push(observeDom(`.custom-theme-background:not([data-${hopefully_unique_id}])`, (elem) => {
-            elem.classList.remove("custom-theme-background");
-            elem.classList.remove("custom-user-profile-theme");
-            elem.style = "--profile-gradient-primary-color: var(--background-surface-high); --profile-gradient-secondary-color: var(--background-surface-high); --profile-gradient-overlay-color: rgba(0, 0, 0, 0); --profile-gradient-button-color: var(--background-mod-subtle); --profile-gradient-modal-background-color: var(--background-base-lower);";
-
-            const mask_circle = elem.querySelector("circle");
-            if (mask_circle) {
-                mask_circle.setAttribute("cx", "56");
-                mask_circle.setAttribute("cy", "101");
-            }
-
+          } else {
             elem.setAttribute(`data-${hopefully_unique_id}`, "true");
-        }));
-    }
+          }
+        }
+      )
+    );
 
-    // gift button
-    store.gift_button ??= true;
-    if (store.gift_button) {
-        cleanup_callbacks.push(observeDom("[aria-label*=gift][class*=button__]", (elem) => {
-            elem.style.display = "none";
-        }));
-    }
-
-    // quests button
-    store.quests_button ??= true;
-    if (store.quests_button) {
-        cleanup_callbacks.push(observeDom("[href*=quest-home]", (elem) => {
-            elem.style.display = "none";
-        }));
-    }
-
-    // boosts button in channel list
-    store.boosts_button ??= true;
-    if (store.boosts_button) {
-        cleanup_callbacks.push(observeDom(`[class*=basicChannelRowLink__]:not([data-${hopefully_unique_id}])`, (elem) => {
-            if (elem.querySelector("[class*=name__] span")?.textContent.includes("Boosts")) {
-                elem.style.display = "none";
-            } else {
-                elem.setAttribute(`data-${hopefully_unique_id}`, "true");
-            }
-        }));
-
-        cleanup_callbacks.push(observeDom("[class*=containerWithMargin__]:has([class*=progressContainer__])", (elem) => {
-            elem.style.display = "none";
-        }));
-    }
+    cleanup_callbacks.push(
+      observeDom(
+        "[class*=containerWithMargin__]:has([class*=progressContainer__])",
+        (elem) => {
+          elem.style.display = "none";
+        }
+      )
+    );
+  }
 }
 
 export function onUnload() {
-    for (const cb of cleanup_callbacks) cb();
+  for (const cb of cleanup_callbacks) cb();
 }
--- plugins/irc-colors/index.tsx
+++ plugins/irc-colors/index.tsx
@@ -1,12 +1,11 @@
 /// <reference path="../../node_modules/@uwu/shelter-defs/dist/shelter-defs/rootdefs.d.ts" />
 
-const { GuildMemberStore, ChannelStore, LayerStore } =
-    shelter.flux.storesFlat;
+const { GuildMemberStore, ChannelStore, LayerStore } = shelter.flux.storesFlat;
 const {
-    util: { getFiber, reactFiberWalker },
-    plugin: { store },
-    ui: { SwitchItem },
-    observeDom,
+  util: { getFiber, reactFiberWalker },
+  plugin: { store },
+  ui: { SwitchItem },
+  observeDom,
 } = shelter;
 
 import { xxHash32 } from "js-xxhash";
@@ -15,74 +14,90 @@ const hopefully_unique_id = "irc-colors-gingeh";
 
 // Calculate a CSS color string based on the user ID
 const calculateNameColorForUser = (() => {
-    const _calculateNameColorForUser = (user_id: string) => {
-        return `oklch(0.750153 0.1275291 ${xxHash32(user_id) % 360})`;
-        //            ^ most vibrant possible hue ring where all hues are in gamut
-    };
+  const _calculateNameColorForUser = (user_id: string) => {
+    return `oklch(0.750153 0.1275291 ${xxHash32(user_id) % 360})`;
+    //            ^ most vibrant possible hue ring where all hues are in gamut
+  };
 
-    const cache = new Map<string, string>();
-    return (user_id: string | null) => {
-        if (!user_id) return null;
-        if (cache.has(user_id)) return cache.get(user_id);
-        const color = _calculateNameColorForUser(user_id);
-        cache.set(user_id, color);
-        return color;
-    };
+  const cache = new Map<string, string>();
+  return (user_id: string | null) => {
+    if (!user_id) return null;
+    if (cache.has(user_id)) return cache.get(user_id);
+    const color = _calculateNameColorForUser(user_id);
+    cache.set(user_id, color);
+    return color;
+  };
 })();
 
 function handleAnyUsername(user_id: string, name_elem: HTMLElement) {
-    if (name_elem.style.color && store.respectRoles) {
-        // name_elem already has a color set directly
-        return;
-    }
+  if (name_elem.style.color && store.respectRoles) {
+    // name_elem already has a color set directly
+    return;
+  }
 
-    const color = calculateNameColorForUser(user_id);
-    if (color) {
-        name_elem.style.color = color;
-    }
+  const color = calculateNameColorForUser(user_id);
+  if (color) {
+    name_elem.style.color = color;
+  }
 }
 
 store.respectRoles ??= true;
 export const settings = () => (
-    <SwitchItem value={store.respectRoles} onChange={(v: boolean) => { store.respectRoles = v }}>
-        Don't override existing role colors.
-    </SwitchItem>
-)
+  <SwitchItem
+    value={store.respectRoles}
+    onChange={(v: boolean) => {
+      store.respectRoles = v;
+    }}
+  >
+    Don't override existing role colors.
+  </SwitchItem>
+);
 
 let unObserveCallbacks: (() => void)[] = [];
 let desaturateClass: string | null = null;
 
 export function onLoad() {
-    if (!desaturateClass) {
-        // desaturateUserColors__XXXXX
-        for (const sheet of document.styleSheets) {
-            for (const rule of sheet.cssRules) {
-                if (!(rule instanceof CSSStyleRule)) continue;
-                let match = rule.selectorText.match(/desaturateUserColors__\w+/);
-                if (match) {
-                    desaturateClass = match[0];
-                    break;
-                }
-            }
+  if (!desaturateClass) {
+    // desaturateUserColors__XXXXX
+    for (const sheet of document.styleSheets) {
+      for (const rule of sheet.cssRules) {
+        if (!(rule instanceof CSSStyleRule)) continue;
+        let match = rule.selectorText.match(/desaturateUserColors__\w+/);
+        if (match) {
+          desaturateClass = match[0];
+          break;
         }
+      }
     }
+  }
 
-    // messages
-    unObserveCallbacks.push(observeDom(`[id*=message-username-]:not([data-${hopefully_unique_id}])`, (elem) => {
+  // messages
+  unObserveCallbacks.push(
+    observeDom(
+      `[id*=message-username-]:not([data-${hopefully_unique_id}])`,
+      (elem) => {
         elem.setAttribute(`data-${hopefully_unique_id}`, "true");
 
         const message = reactFiberWalker(getFiber(elem), "message", true)
-            ?.pendingProps?.message;
+          ?.pendingProps?.message;
         if (!message) return;
 
-        if (store.respectRoles && !LayerStore.getLayers().includes('USER_SETTINGS')) {
-            const { type, guild_id } = ChannelStore.getChannel(message.channel_id);
-            if (type === 0) {
-                // message in a guild, check if user has a role color
-                const member = GuildMemberStore.getMember(guild_id, message.author.id);
-                if (!member) return;
-                if (member.colorString !== null) return; // user has a manual color set
-            }
+        if (
+          store.respectRoles &&
+          !LayerStore.getLayers().includes("USER_SETTINGS")
+        ) {
+          const { type, guild_id } = ChannelStore.getChannel(
+            message.channel_id
+          );
+          if (type === 0) {
+            // message in a guild, check if user has a role color
+            const member = GuildMemberStore.getMember(
+              guild_id,
+              message.author.id
+            );
+            if (!member) return;
+            if (member.colorString !== null) return; // user has a manual color set
+          }
         }
 
         const usernameElem = elem.firstElementChild;
@@ -90,30 +105,44 @@ export function onLoad() {
 
         handleAnyUsername(message.author.id, usernameElem);
         if (desaturateClass) usernameElem.classList.add(desaturateClass);
-    }));
+      }
+    )
+  );
 
-    // member list
-    unObserveCallbacks.push(observeDom(`[class*=nameContainer__]:not([data-${hopefully_unique_id}])`, (elem) => {
+  // member list
+  unObserveCallbacks.push(
+    observeDom(
+      `[class*=nameContainer__]:not([data-${hopefully_unique_id}])`,
+      (elem) => {
         elem.setAttribute(`data-${hopefully_unique_id}`, "true");
 
-        const user = reactFiberWalker(getFiber(elem), "user", true)?.pendingProps?.user;
+        const user = reactFiberWalker(getFiber(elem), "user", true)
+          ?.pendingProps?.user;
         if (!user) return;
 
         handleAnyUsername(user.id, elem as HTMLElement);
-    }));
+      }
+    )
+  );
 
-    // replied message authors
-    unObserveCallbacks.push(observeDom(`[class*=repliedMessage_] [class*=username_]:not([data-${hopefully_unique_id}])`, (elem) => {
+  // replied message authors
+  unObserveCallbacks.push(
+    observeDom(
+      `[class*=repliedMessage_] [class*=username_]:not([data-${hopefully_unique_id}])`,
+      (elem) => {
         elem.setAttribute(`data-${hopefully_unique_id}`, "true");
 
-        const user = reactFiberWalker(getFiber(elem), "user", true)?.pendingProps?.user;
+        const user = reactFiberWalker(getFiber(elem), "user", true)
+          ?.pendingProps?.user;
         if (!user) return;
 
         handleAnyUsername(user.id, elem as HTMLElement);
         if (desaturateClass) elem.classList.add(desaturateClass);
-    }));
+      }
+    )
+  );
 }
 
 export function onUnload() {
-    for (const unObserve of unObserveCallbacks) unObserve();
+  for (const unObserve of unObserveCallbacks) unObserve();
 }
